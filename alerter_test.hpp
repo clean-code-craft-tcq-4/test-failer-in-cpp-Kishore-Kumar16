c34e85541dd7d2a4d6e3aaf65cd7e532054d37bf
Kishore <Kishorekumar161197@gmail.com>
2022-06-27 21:58:45 +0530

    Resolved separate assert from functionality
    fixed linker issue

---
+++ alerter_test.hpp
@@ -0,0 +1,12 @@
+#ifndef ALERTER_TEST_H_
+#define ALERTER_TEST_H_
+
+#include <iostream>
+
+#include "alerter.hpp"
+
+namespace alerter_stub {
+
+    int networkAlertStub(float celius);
+};
+#endif //ALERTER_TEST_H_
